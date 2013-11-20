636cf971d744beb00860a674282864c6d12b45db
Chris Irish <supairish@gmail.com>
2013-11-19 18:01:47 -0700

    more spec tweakage

--- spec/controllers/orders_controller_spec.rb
+++ spec/controllers/orders_controller_spec.rb
@@ -9,7 +9,7 @@ describe OrdersController do
     end
   end
 
-  describe 'Index PDF' do
+  describe 'Index PDF headers & templates' do
     render_views
 
     let(:filename){ "Orders-for-#{Time.zone.now.strftime("%m-%d-%Y")}.pdf" }
@@ -17,10 +17,15 @@ describe OrdersController do
     it "should render the home page template in the application layout" do
       get :index, format: 'pdf'
 
-      expect(response.content_type).to eq("application/pdf")
-      expect(response.headers["Content-Disposition"]).to eq("attachment; filename=\"#{filename}\"")
       expect(response).to render_template('orders/index')
       expect(response).to render_template(layout: "print.html")
+    end
+
+    it "should render with the correct headers" do
+      get :index, format: 'pdf'
+
+      expect(response.content_type).to eq("application/pdf")
+      expect(response.headers["Content-Disposition"]).to eq("attachment; filename=\"#{filename}\"")
     end
 
   end
@@ -28,10 +33,11 @@ describe OrdersController do
   describe "Index PDF content" do
     render_views
 
-    it "should description" do
+    it "should have a table header and the correct table contents" do
       get :index, format: 'pdf', debug: '1'
 
       page = Capybara::Node::Simple.new(response.body)
+
       expect(page).to have_selector("thead th", text: 'Title')
       expect(page).to have_selector("thead th", text: 'Total')
       expect(page).to have_selector("tbody tr", count: 10 )
